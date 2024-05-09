4a5947d92c2c251f3de7f98ef844fb74530c2b74
HenriMykkanen <144138353+HenriMykkanen@users.noreply.github.com>
2024-05-09 18:25:16 +0300

    Delete db.php

--- lib/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql = new mysqli("localhost", "henri_mykkanen", "sg1Ka0_2", "sakky_henri_mykkanen");
-
-if($sql->connect_errno) {
-    die("Problems occurred while trying to connect to the database " . $sql->connect_error);
-}
-
-?>
